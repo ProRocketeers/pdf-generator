c3b2db9d4721491dbb9e8c0bf7e39158522dcf3d
Jakub Kleberc <jakub.kleberc@tacr-external.cz>
2025-06-03 10:28:47 +0200

    feat: init configs and app

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,34 +1,42 @@
 // @ts-check
-import eslint from '@eslint/js'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginPromise from 'eslint-plugin-promise';
+import { globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
+  globalIgnores(['**/node_modules', '**/dist', '**/build', '**/webpack.config.cjs']),
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+  tseslint.configs.recommended,
+  pluginPromise.configs['flat/recommended'],
+  eslintConfigPrettier,
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'jsx-quotes': ['warn', 'prefer-double'],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false,
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^(_|ignore)',
+        },
+      ],
+      'no-restricted-imports': [
+        'error',
+        {
+          name: 'react-i18next',
+          message:
+            'Please use custom `hooks/useTranslation` instead of `react-i18next` directly.',
+        },
+      ],
     },
   },
-)
+);
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { configureSwagger } from 'src/configs/swagger.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3001)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  configureSwagger(app);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap()
+bootstrap();
