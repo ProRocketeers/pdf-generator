4e5935c8aca04d9b6e3ca2fced88d5a7f3fa9800
Jakub Kleberc <jakub.kleberc@tacr-external.cz>
2025-06-30 16:29:42 +0200

    feat: add get template endpoint

--- backend/src/template/template.controller.ts
+++ backend/src/template/template.controller.ts
@@ -1,13 +1,22 @@
 import { GetTemplateListService } from '@backend/template/getTemplateList.service';
+import { GetTemplateService } from '@backend/template/getTemplateservice';
 import { TemplateDto } from '@backend/template/template.dto';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('api/v1/template')
 export class TemplateController {
-  constructor(private readonly getTemplateListService: GetTemplateListService) {}
+  constructor(
+    private readonly getTemplateListService: GetTemplateListService,
+    private readonly getTemplateService: GetTemplateService,
+  ) {}
 
   @Get()
   async getList(): Promise<TemplateDto[]> {
     return this.getTemplateListService.getTemplateList();
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: string): Promise<TemplateDto> {
+    return this.getTemplateService.getTemplateList(id);
   }
 }
--- backend/src/template/template.module.ts
+++ backend/src/template/template.module.ts
@@ -1,4 +1,5 @@
 import { GetTemplateListService } from '@backend/template/getTemplateList.service';
+import { GetTemplateService } from '@backend/template/getTemplateservice';
 import { TemplateController } from '@backend/template/template.controller';
 import { Template } from '@backend/template/template.entity';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
@@ -7,6 +8,6 @@ import { Module } from '@nestjs/common';
 @Module({
   imports: [MikroOrmModule.forFeature([Template])],
   controllers: [TemplateController],
-  providers: [GetTemplateListService],
+  providers: [GetTemplateListService, GetTemplateService],
 })
 export class TemplateModule {}
