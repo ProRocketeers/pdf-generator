3f535ed6a710a08a39fcf0eba35f982c95418b22
Ondra Sýkora <ondrej@sykora.it>
2025-06-30 20:05:16 +0200

    feat: update environment configuration and enhance Swagger setup

--- backend/src/configs/swagger.config.ts
+++ backend/src/configs/swagger.config.ts
@@ -1,14 +1,15 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../../package.json';
 
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('PDF Generator')
-  .setVersion('1.0')
+  .setVersion(version)
   .build();
 
 export const configureSwagger = (app: NestExpressApplication) => {
   const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api/v1/swagger', app, documentFactory, {
-    yamlDocumentUrl: 'api/v1/swagger/yaml',
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    yamlDocumentUrl: 'swagger/yaml',
   });
 };
