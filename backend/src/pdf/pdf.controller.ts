b8f5fea62fdc06b3173b71acc4b4f800b9cde0a4
Ondra Sýkora <ondrej@sykora.it>
2025-07-01 21:49:50 +0200

    refactor: improve ApiBody definition in createPdf method and clean up code formatting

--- backend/src/pdf/pdf.controller.ts
+++ backend/src/pdf/pdf.controller.ts
@@ -1,35 +1,51 @@
-import { CreatePdfRequestDto } from '@backend/pdf/createPdfRequest.dto';
-import { PdfService } from '@backend/pdf/service/pdf.service';
-import { GetTemplateService } from '@backend/template/getTemplateService';
-import { Body, Controller, Post, StreamableFile } from '@nestjs/common';
-import { ApiBody } from '@nestjs/swagger';
+import { CreatePdfRequestDto } from '@backend/pdf/createPdfRequest.dto'
+import { PdfService } from '@backend/pdf/service/pdf.service'
+import { GetTemplateService } from '@backend/template/getTemplateService'
+import { Body, Controller, Post, StreamableFile } from '@nestjs/common'
+import { ApiBody } from '@nestjs/swagger'
 
 @Controller('api/v1/pdf')
 export class PdfController {
   constructor(
     private readonly getTemplateService: GetTemplateService,
     private readonly pdfApiService: PdfService,
-  ) {}
+  ) { }
 
   @Post()
-  @ApiBody({ type: CreatePdfRequestDto })
+  @ApiBody({
+    type: CreatePdfRequestDto,
+    examples: {
+      'default': {
+        value: {
+          templateId: "58467411-5729-488e-88cf-30bbf239fbe7",
+          variables: {
+            amount: "100.00",
+            currency: "EUR",
+            date: "2025-05-04",
+            name: "Ondra Doe",
+            reference: "ABC123"
+          }
+        }
+      },
+    }
+  })
   async createPdf(
     @Body() createPdfRequestDto: CreatePdfRequestDto,
   ): Promise<StreamableFile> {
-    const { templateId, variables, fileName } = createPdfRequestDto;
+    const { templateId, variables, fileName } = createPdfRequestDto
 
-    const template = await this.getTemplateService.getTemplate(templateId);
+    const template = await this.getTemplateService.getTemplate(templateId)
     if (!template) {
-      throw new Error(`Template with id ${templateId} not found`);
+      throw new Error(`Template with id ${templateId} not found`)
     }
 
-    const { templateUrl, templateType } = template;
+    const { templateUrl, templateType } = template
 
     const pdfStream = await this.pdfApiService.generatePdf({
       templateType,
       templateUrl,
       variables,
-    });
+    })
 
     // Dynamically set headers using the response object
     // Inject Response from @nestjs/common
@@ -42,7 +58,7 @@ export class PdfController {
     const streamableFile = new StreamableFile(pdfStream, {
       type: 'application/pdf',
       disposition: `attachment; filename="${fileName || 'generated.pdf'}"`,
-    });
-    return streamableFile;
+    })
+    return streamableFile
   }
 }
