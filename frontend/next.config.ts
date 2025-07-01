561a4007513e5708ae463f431c8ff6fb6036bee5
Ondra Sýkora <ondrej@sykora.it>
2025-07-01 22:03:47 +0200

    fix: add assetPrefix configuration to next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 export default nextConfig
