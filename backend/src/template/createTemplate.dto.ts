592d6bc0cce4a660560019b8c405e0af085d478f
Ondra Sýkora <ondrej@sykora.it>
2026-03-21 21:27:22 +0100

    feat: redefine CreateTemplateDto and CreateVariableDto with explicit properties and validation

--- backend/src/template/createTemplate.dto.ts
+++ backend/src/template/createTemplate.dto.ts
@@ -1,4 +1,45 @@
-import { OmitType } from '@nestjs/swagger'
-import { TemplateDto } from './template.dto'
+import { TemplateType } from '@backend/template/service/templateType.enum'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator'
 
-export class CreateTemplateDto extends OmitType(TemplateDto, ['id', 'variables'] as const) { }
+export class CreateTemplateDto {
+	@ApiProperty({
+		description: 'Title of the template',
+		example: 'Payment Receipt',
+	})
+	@IsString()
+	@IsNotEmpty()
+	title: string
+
+	@ApiProperty({
+		description: 'Description of the template',
+		example: 'Description of PDF Template',
+	})
+	@IsString()
+	@IsNotEmpty()
+	description: string
+
+	@ApiProperty({
+		description: 'URL to the template file',
+		example: 'https://drive.google.com/uc?export=download&id=16oauTQqVnJtJEl8unMYyUpH6BILRS97C',
+	})
+	@IsString()
+	@IsNotEmpty()
+	templateUrl: string
+
+	@ApiProperty({
+		description: 'Type of the template',
+		enum: TemplateType,
+		example: TemplateType.Adoc,
+	})
+	@IsEnum(TemplateType)
+	templateType: TemplateType
+
+	@ApiProperty({
+		description: 'URL to the image representing the template',
+		example: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1667px-PDF_file_icon.svg.png',
+	})
+	@IsString()
+	@IsNotEmpty()
+	imageUrl: string
+}
--- backend/src/template/createVariable.dto.ts
+++ backend/src/template/createVariable.dto.ts
@@ -1,4 +1,39 @@
-import { OmitType } from '@nestjs/swagger'
-import { VariableDto } from './variable.dto'
+import { VariableType } from '@backend/template/service/variableType.enum'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 
-export class CreateVariableDto extends OmitType(VariableDto, ['id'] as const) { }
+export class CreateVariableDto {
+	@ApiProperty({
+		description: 'Variable name',
+		example: 'amount',
+	})
+	@IsString()
+	@IsNotEmpty()
+	name: string
+
+	@ApiProperty({
+		description: 'Variable type',
+		enum: VariableType,
+		example: VariableType.String,
+	})
+	@IsEnum(VariableType)
+	type: VariableType
+
+	@ApiProperty({
+		description: 'Variable title',
+		example: 'Amount',
+	})
+	@IsString()
+	@IsNotEmpty()
+	title: string
+
+	@ApiProperty({
+		description: 'Default value',
+		example: '104.00',
+		nullable: true,
+		required: false,
+	})
+	@IsOptional()
+	@IsString()
+	default?: string | null
+}
