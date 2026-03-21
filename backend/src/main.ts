40e176397908502e6d4c815d21d5aec586e793e5
Ondra Sýkora <ondrej@sykora.it>
2026-03-21 22:14:12 +0100

    refactor: clean up main.ts by removing semicolons and improving formatting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
-import { AppModule } from '@backend/app.module';
-import { configureSwagger } from '@backend/configs/swagger.config';
-import { GlobalExceptionFilter } from '@backend/filters/global-exception.filter';
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe'
+import { AppModule } from '@backend/app.module'
+import { configureSwagger } from '@backend/configs/swagger.config'
+import { GlobalExceptionFilter } from '@backend/filters/global-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const port = Number(process.env.PORT || 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const port = Number(process.env.PORT || 3000)
 
-  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.enableCors()
+
+  app.useGlobalFilters(new GlobalExceptionFilter())
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
+  )
 
-  configureSwagger(app);
+  configureSwagger(app)
 
-  await app.listen(port);
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
