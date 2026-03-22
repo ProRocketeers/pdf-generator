8389267e6c7237834d2b5f834ee8970906037b76
Ondra Sýkora <ondrej@sykora.it>
2026-03-22 12:17:29 +0100

    feat: enhance bootstrap function with logging and improved validation pipe configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe'
+import { Logger, ValidationPipe } from '@nestjs/common'
 import { AppModule } from '@backend/app.module'
 import { configureSwagger } from '@backend/configs/swagger.config'
 import { GlobalExceptionFilter } from '@backend/filters/global-exception.filter'
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap')
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   const port = Number(process.env.PORT || 3000)
 
@@ -13,12 +14,26 @@ async function bootstrap() {
 
   app.useGlobalFilters(new GlobalExceptionFilter())
 
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  )
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }))
 
   configureSwagger(app)
 
+  app.use((req, res, next) => {
+    if (req.method === 'GET' && req.path === '/') {
+      res.redirect('/swagger')
+      return
+    }
+
+    next()
+  })
+
   await app.listen(port)
+
+  const localUrl = new URL(await app.getUrl())
+  logger.log(`Application is running on: http://localhost:${localUrl.port}`)
 }
 bootstrap()
